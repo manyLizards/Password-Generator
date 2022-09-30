153b8a9c07417f37882224d9c4b70959f15bdb5b
Jordan <jnbarringer96@gmail.com>
2022-09-30 11:34:54 -0500

    added challenge files and initial variables

---
+++ script.js
@@ -0,0 +1,41 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Lines 5-27 Code learned from Youtube's Traversy Media from video "Javascript Password Generator"
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+}
+
+function getRandomLower() {
+  return String.fromCharCode (Math.floor(Math.random() * 26 + 97));
+}
+
+function getRandomUpper() {
+  return String.fromCharCode (Math.floor(Math.random() * 26 + 65));
+}
+
+function getRandomNumber() {
+  return String.fromCharCode (Math.floor(Math.random() * 10 + 48));
+}
+
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*(){}[]=<>/,.'
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+console.log(getRandomLower());

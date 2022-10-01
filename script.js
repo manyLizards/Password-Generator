aad1a3344c19adce56ba50e5f707e349f64c4808
Jordan <jnbarringer96@gmail.com>
2022-10-01 12:55:27 -0500

    added html form and initial javascript for the form

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Lines 5-27 Code learned from Youtube's Traversy Media from video "Javascript Password Generator"
+var formEl = document.querySelector("#preferences")
+
+var submitBtn = document.querySelector("#submit")
+
+//create an empty object to store preferences
+var preferences = {}
+
+// Lines 12-33 Code learned from Youtube's Traversy Media from video "Javascript Password Generator"
 const randomFunc = {
   lower: getRandomLower,
   upper: getRandomUpper,
@@ -26,6 +33,30 @@ function getRandomSymbol() {
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
+function getPreferences(event) {
+  event.preventDefault()
+  //hide preferences form
+  formEl.style.display = "none";
+  var leng = document.querySelector("#length").value
+  var lowercase = document.querySelector("#lowercase").checked
+  var uppercase = document.querySelector("#uppercase").checked
+  var numbers = document.querySelector("#numbers").checked
+  var special = document.querySelector("#special").checked
+  //assign preference variables as keys to preferences object
+  preferences.leng = leng
+  preferences.lowercase = lowercase
+  preferences.uppercase = uppercase
+  preferences.numbers = numbers
+  preferences.special = special
+}
+
+function generatePassword() {
+  //need to display HTML element after pressing the submit button rather than return immediately
+  formEl.style.display = "block";
+  var pwd = "dfjeioe"
+  return pwd
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -38,4 +69,4 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-console.log(getRandomLower());
+submitBtn.addEventListener("click", getPreferences)
